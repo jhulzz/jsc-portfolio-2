7f8a0b56abf91fb060d42a316bcc033e529afbc6
jcantada1 <jcantada@gmail.com>
2017-11-23 14:58:58 +0800

    with contact form

--- index.php
+++ index.php
@@ -1,28 +1,3 @@
-
-
-<form action="test.php" method="post">
-<table width="400" border="0" cellspacing="2" cellpadding="0">
-<tr>
-<td width="29%" class="bodytext">Your name:</td>
-<td width="71%"><input name="name" type="text" id="name" size="32"></td>
-</tr>
-<tr>
-<td class="bodytext">Email address:</td>
-<td><input name="email" type="text" id="email" size="32"></td>
-</tr>
-<tr>
-<td class="bodytext">Comment:</td>
-<td><textarea name="comment" cols="45" rows="6" id="comment" class="bodytext"></textarea></td>
-</tr>
-<tr>
-<td class="bodytext"> </td>
-<td align="left" valign="top"><input type="submit" name="Submit" value="Send"></td>
-</tr>
-</table>
-</form> 
-
-
-
 <!DOCTYPE html>
 <html>
 
@@ -481,49 +456,47 @@
                         <div class="single contact-info">
                         <h3 class="side-title">drop a message</h3>
 
-                        <?php 
-                        if ($_POST["email"]<>'') { 
-                            $ToEmail = 'jcantada@gmail.com'; 
-                            $EmailSubject = 'Inquiry from JSC Portfolio'; 
-                            $mailheader = "From: ".$_POST["email"]."\r\n"; 
-                            $mailheader .= "Reply-To: ".$_POST["email"]."\r\n"; 
-                            $mailheader .= "Content-type: text/html; charset=iso-8859-1\r\n"; 
-                            $MESSAGE_BODY = "Name: ".$_POST["name"].""; 
-                            $MESSAGE_BODY .= "Email: ".$_POST["email"].""; 
-                            $MESSAGE_BODY .= "Message: ".nl2br($_POST["message"]).""; 
-                            mail($ToEmail, $EmailSubject, $MESSAGE_BODY, $mailheader) or die ("Failure"); 
-                        ?> 
+                        <?php
 
-                        Your message was sent
+                        $name = trim($_POST['name']);
+                        $email = trim($_POST['email']);
+                        $comments = trim($_POST['message']);
 
-                        <?php 
-                        } else { 
+                        $emailTo = 'jcantada@gmail.com';
+                        $subject = 'Submitted message from '.$name;
+                        $body = "Name: $name \n\nEmail: $email \n\nComments: $comments";
+                        $headers = 'From: ' .' <'.$emailTo.'>' . "\r\n" . 'Reply-To: ' . $email;
+
+                        {
+
+                          mail($emailTo, $subject, $body, $headers);
+                          echo 'Message sent';
+                        }
+                        
                         ?>
 
-                        <form action="index.html" method="POST">
-                            <ul class="list-unstyled">
-                            <li>
-                            <input id="name" name="name" type="text" class="contactfield" placeholder="Your Name">
-                            </li>
-                            <li>
-                                <input id="email" name="email" type="email" class="contactfield" placeholder="Your Email address">
-                            </li>
+                            <form class="form-horizontal" role="form" method="post" action="sndmail.php">
+                                    <ul class="list-unstyled">
+                                    <li>
+                                    <input id="name" name="name" type="text" class="contactfield" placeholder="Your Name">
+                                    </li>
+                                    
+                                    <li>
+                                        <input id="email" name="email" type="email" class="contactfield" placeholder="Your Email address">   
+                                        
+                                    </li>
 
-                            <li>
-                            <textarea id="message" name="message" class="contactfield" placeholder="Sample message: Please send in your complete CV." rows="5"></textarea>
-                            </li>
+                                    <li>
+                                    <textarea id="message" name="message" class="contactfield" placeholder="Sample message: Please send in your complete CV." rows="5"></textarea>
+                                    </li>
 
-                            <li>
-                             <a id="contact-submit" type="submit" class="btn btn-default input-medium">Send</a>
+                                    <li>
+                                     <a id="contact-submit" type="submit" class="btn btn-default input-medium">Send</a>
 
-                            </li>
-                            </ul>
-                    </form>
-
-                    <?php 
-                    }; 
-                    ?>
-
+                                    </li>
+                                    </ul>
+                            </form>
+                         
                         </div>
                     </aside>
                 </div>
---
+++ sndmail.php
@@ -0,0 +1,15 @@
+<?php
+$name = trim($_POST['name']);
+$email = trim($_POST['email']);
+$comments = trim($_POST['message']);
+
+  $emailTo = 'your@email.com';
+  $subject = 'Submitted message from '.$name;
+  $body = "Name: $name \n\nEmail: $email \n\nComments: $comments";
+  $headers = 'From: ' .' <'.$emailTo.'>' . "\r\n" . 'Reply-To: ' . $email;
+
+  mail($emailTo, $subject, $body, $headers);
+  echo 'email sent..';
+
+
+?>
